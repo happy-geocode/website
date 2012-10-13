6d288cea6303cb7a2df16e47d6c098bd32e05346
Tobias Eilert <tobias.eilert@tux-mail.de>
2012-10-13 18:45:16 +0200

    Insert awsum pics for promo boxes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,24 +5,28 @@ class WelcomeController < ApplicationController
       {
         name: "bitboxer",
         github: "https://github.com/bitboxer",
+        twitter: "http://twitter.com/bitboxer",
         desc: %q{Owns a colorful collection of crayons},
         img: 'bitboxer.jpg'
       },
       {
         name: "klaustopher",
         github: "https://github.com/klaustopher",
+        twitter: "http://twitter.com/klaustopher",
         desc: "Chocolate for the mob",
         img: 'klaustopher.jpg'
       },
       {
         name: "moonglum",
         github: "https://github.com/moonglum",
+        twitter: "http://twitter.com/moonbeamlabs",
         desc: "Replaced his hair with <3",
         img: 'moonglum.jpg'
       },
       {
         name: "EinLama",
         github: "https://github.com/EinLama",
+        twitter: "http://twitter.com/l_ama",
         desc: "Trying to minimize visual catastrophies",
         img: 'EinLama.jpg'
       }
