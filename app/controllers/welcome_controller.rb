785d6d212fc4c401b791329a309ef40064e66359
Tobias Eilert <tobias.eilert@tux-mail.de>
2012-10-13 15:10:31 +0200

    Show our great team

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,31 @@ class WelcomeController < ApplicationController
   end
 
   def about
+    @team_members = [
+      {
+        name: "bitboxer",
+        github: "https://github.com/bitboxer",
+        desc: %q{Bodobodododo is not Frododododo},
+        img: 'bitboxer.jpg'
+      },
+      {
+        name: "klaustopher",
+        github: "https://github.com/klaustopher",
+        desc: "Chocolate for the mob",
+        img: 'klaustopher.jpg'
+      },
+      {
+        name: "moonglum",
+        github: "https://github.com/moonglum",
+        desc: "Replaced his hair with <3",
+        img: 'moonglum.jpg'
+      },
+      {
+        name: "EinLama",
+        github: "https://github.com/EinLama",
+        desc: "Trying to minimize visual catastrophies",
+        img: 'EinLama.jpg'
+      }
+    ]
   end
 end
