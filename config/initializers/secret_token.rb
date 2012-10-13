cf999b1ffd4be5fba004e180d8351b559f4ea511
Klaus Zanders <klaus.zanders@gmail.com>
2012-10-13 08:34:30 +0200

    Initial commit with Rails, Bootstrap, Leaflet, Fontawesome

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyGeocode::Application.config.secret_token = '570abda171b302a50cc3b468200541e0a48581cf1c89eafebbecfdcbfae0a330c0edda58250b7be257d2bbc448e22945f6eb23ac5f538b2a7d65608060a5f5f3'
