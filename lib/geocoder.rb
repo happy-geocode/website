d724d855f6db1d9b38d03ef4bf76f2a1d4e46e46
Tobias Eilert <tobias.eilert@tux-mail.de>
2012-10-14 19:16:56 +0200

    Implement basic search functionality
    Currently breaks since the models are not ready yet.
    
    @moonglum is working on it right now!

--- lib/geocoder.rb
+++ lib/geocoder.rb
@@ -12,24 +12,72 @@ class Geocoder
   def find(params)
     return [] if params[:query].blank?
     parsed = Parsec.parse params[:query]
+
     enrich_parsed parsed
   end
 
   private
 
   def enrich_parsed(parsed)
-    # ToDo: Add actual searching in arango :)
-    [{
-      street_name: parsed.street_name,
-      street_number: parsed.street_number,
-      zip: parsed.zip,
-      city: parsed.city,
-      state: parsed.state,
-      country: "Germany",
-      lat: 50.937229,
-      lon: 6.832788,
-      accuracy: 'exact',
-      accuracy_face: ":)"
-    }]
+    #[{
+      #street_name: parsed.street_name,
+      #street_number: parsed.street_number,
+      #zip: parsed.zip,
+      #city: parsed.city,
+      #state: parsed.state,
+      #country: "Germany",
+      #lat: 50.937229,
+      #lon: 6.832788,
+      #accuracy: 'exact',
+      #accuracy_face: ":)"
+    #}]
+
+    # Try to find by street
+    found_entries = find_streets(parsed)
+
+    # Continue searching if we didn't find anything
+    if found_entries.empty?
+      found_entries.concat find_zip_matches(parsed.zip) if parsed.zip
+      found_entries.concat find_city_matches(parsed.city) if parsed.city
+    end
+
+    found_entries
   end
+
+  def find_streets(parsed)
+    unless parsed.street_name
+      return []
+    end
+
+    search_options = {}
+    search_options[:name] = parsed.street_name
+    search_options[:zip] = parsed.zip if parsed.zip
+    search_options[:city] = parsed.city if parsed.city
+
+    street_matches = Street.find(search_options)
+
+    street_matches.map do |s|
+      s[:accuracy] = 'street'
+      s[:accuracy_face] = ':)'
+    end
+  end
+
+  def find_zip_matches(zip)
+    zip_matches = Zip.find_by_name(zip)
+
+    zip_matches.map do |m|
+      m[:accuracy] = 'zip'
+      m[:accuracy_face] = ':/'
+    end
+  end
+
+  def find_city_matches(city)
+    city_matches = City.find_by_name(city)
+
+    city_matches.map do |m|
+      m[:accuracy] = 'city'
+      m[:accuracy_face] = ':O'
+    end
+  end
+
 end
